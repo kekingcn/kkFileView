02e116fd8aecee5969961b6dcd58305d7eb45d20
陈精华 <842761733@qq.com>
2019-04-14 22:45:18 +0800

    完善启动脚本

--- jodconverter-web/src/main/bin/startup.bat
+++ jodconverter-web/src/main/bin/startup.bat
@@ -1,1 +1,7 @@
+@echo off
+set "KKFILEVIEW_BIN_FOLDER=%cd%"
+cd "%KKFILEVIEW_BIN_FOLDER%"
+echo Using KKFILEVIEW_BIN_FOLDER %KKFILEVIEW_BIN_FOLDER%
+echo Starting kkFileView...
+echo Please check log file for more information
 java -Dspring.config.location=..\conf\application.properties -jar jodconverter-web-1.5.8.RELEASE.jar -> ..\log\kkFileView.log
--- jodconverter-web/src/main/bin/startup.sh
+++ jodconverter-web/src/main/bin/startup.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+KKFILEVIEW_BIN_FOLDER=$(cd "$(dirname "$0")";pwd)
+export KKFILEVIEW_BIN_FOLDER=$KKFILEVIEW_BIN_FOLDER
+cd $KKFILEVIEW_BIN_FOLDER
+echo "Using KKFILEVIEW_BIN_FOLDER $KKFILEVIEW_BIN_FOLDER"
+echo "Starting kkFileView..."
+echo "Please check log file for more information"
 nohup java -Dspring.config.location=../conf/application.properties -jar jodconverter-web-1.5.8.RELEASE.jar ../log/kkFileView.log 2>&1 &
