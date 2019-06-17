440b8030e045aa0369ad5b5a582e4e90a57603f7
陈精华 <842761733@qq.com>
2019-06-17 18:13:34 +0800

    新增Docker构建

---
+++ jodconverter-web/src/main/bin/docker-startup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+DIR_HOME=("/opt/openoffice.org3" "/opt/libreoffice" "/opt/openoffice4" "/usr/lib/openoffice" "/usr/lib/libreoffice")
+FLAG=
+OFFICE_HOME=
+KKFILEVIEW_BIN_FOLDER=$(cd "$(dirname "$0")";pwd)
+export KKFILEVIEW_BIN_FOLDER=$KKFILEVIEW_BIN_FOLDER
+cd $KKFILEVIEW_BIN_FOLDER
+echo "Using KKFILEVIEW_BIN_FOLDER $KKFILEVIEW_BIN_FOLDER"
+grep 'office\.home' ../conf/application.properties | grep '!^#'
+if [ $? -eq 0 ]; then
+  echo "Using customized office.home"
+else 
+ for i in ${DIR_HOME[@]}
+  do
+    if [ -f $i"/program/soffice.bin" ]; then
+      FLAG=true
+      OFFICE_HOME=${i}
+      break
+    fi
+  done
+  if [ ! -n "${FLAG}" ]; then
+    echo "Installing OpenOffice"
+    sh ../script/install.sh
+  else 
+    echo "Detected office component has been installed in $OFFICE_HOME"
+  fi
+fi
+echo "Starting kkFileView..."
+java -Dsun.java2d.cmm=sun.java2d.cmm.kcms.KcmsServiceProvider -Dspring.config.location=../conf/application.properties -jar kkFileView-2.1.0-SNAPSHOT.jar
