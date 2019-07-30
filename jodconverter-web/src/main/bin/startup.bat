8798d344b6513106af39f1872f531f3e53bb3b7e
陈精华 <842761733@qq.com>
2019-07-30 14:48:28 +0800

    2.1.2版

--- jodconverter-web/src/main/bin/startup.bat
+++ jodconverter-web/src/main/bin/startup.bat
@@ -4,4 +4,4 @@ cd "%KKFILEVIEW_BIN_FOLDER%"
 echo Using KKFILEVIEW_BIN_FOLDER %KKFILEVIEW_BIN_FOLDER%
 echo Starting kkFileView...
 echo Please check log file for more information
-java -Dsun.java2d.cmm=sun.java2d.cmm.kcms.KcmsServiceProvider -Dspring.config.location=..\conf\application.properties -jar kkFileView-2.1.1.jar -> ..\log\kkFileView.log
+java -Dsun.java2d.cmm=sun.java2d.cmm.kcms.KcmsServiceProvider -Dspring.config.location=..\conf\application.properties -jar kkFileView-2.1.2.jar -> ..\log\kkFileView.log
