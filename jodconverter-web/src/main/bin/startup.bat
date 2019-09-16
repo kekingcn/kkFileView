6df2b6d0a429bf19bf575750e18b3f7f6382544f
陈精华 <842761733@qq.com>
2019-09-16 11:33:06 +0800

    优化：启动脚本加入官网链接，点star推广等文案 close #I1148F

--- jodconverter-web/src/main/bin/startup.bat
+++ jodconverter-web/src/main/bin/startup.bat
@@ -3,5 +3,7 @@ set "KKFILEVIEW_BIN_FOLDER=%cd%"
 cd "%KKFILEVIEW_BIN_FOLDER%"
 echo Using KKFILEVIEW_BIN_FOLDER %KKFILEVIEW_BIN_FOLDER%
 echo Starting kkFileView...
-echo Please check log file for more information
+echo Please check log file in ./log/kkFileView.log for more information
+echo You can get help in our official homesite: https://kkFileView.keking.cn
+echo If this project is helpful to you, please star it in https://gitee.com/kekingcn/file-online-preview
 java -Dsun.java2d.cmm=sun.java2d.cmm.kcms.KcmsServiceProvider -Dspring.config.location=..\conf\application.properties -jar kkFileView-2.2.0-SNAPSHOT.jar -> ..\log\kkFileView.log
