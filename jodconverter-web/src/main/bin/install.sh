59f7ae53089d8c4164ba915e46c843755e8b4f97
陈精华 <842761733@qq.com>
2019-11-29 14:39:13 +0800

    脚本：使用kkFileView站点下载OpenOffice安装文件，加速国内用户构建Docker镜像

--- jodconverter-web/src/main/bin/install.sh
+++ jodconverter-web/src/main/bin/install.sh
@@ -2,7 +2,7 @@
 cd /tmp
 
 install_redhat() {
-    wget https://iweb.dl.sourceforge.net/project/openofficeorg.mirror/4.1.6/binaries/zh-CN/Apache_OpenOffice_4.1.6_Linux_x86-64_install-rpm_zh-CN.tar.gz -cO openoffice_rpm.tar.gz && tar zxf /tmp/openoffice_rpm.tar.gz && cd /tmp/zh-CN/RPMS
+    wget https://kkfileview.keking.cn/Apache_OpenOffice_4.1.6_Linux_x86-64_install-rpm_zh-CN.tar.gz -cO openoffice_rpm.tar.gz && tar zxf /tmp/openoffice_rpm.tar.gz && cd /tmp/zh-CN/RPMS
    if [ $? -eq 0 ];then
      yum install -y libXext.x86_64
      yum groupinstall -y  "X Window System"
@@ -16,7 +16,7 @@ install_redhat() {
 }
 
 install_ubuntu() {
-   wget  https://iweb.dl.sourceforge.net/project/openofficeorg.mirror/4.1.6/binaries/zh-CN/Apache_OpenOffice_4.1.6_Linux_x86-64_install-deb_zh-CN.tar.gz  -cO openoffice_deb.tar.gz && tar zxf /tmp/openoffice_deb.tar.gz && cd /tmp/zh-CN/DEBS
+   wget  https://kkfileview.keking.cn/Apache_OpenOffice_4.1.6_Linux_x86-64_install-deb_zh-CN.tar.gz  -cO openoffice_deb.tar.gz && tar zxf /tmp/openoffice_deb.tar.gz && cd /tmp/zh-CN/DEBS
    echo $?
  if [ $? -eq 0 ];then
      dpkg -i *.deb
