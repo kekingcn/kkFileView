f92fe73389a3c1fc783af9ba3ec6ce225f45719b
陈精华 <842761733@qq.com>
2020-05-19 11:04:53 +0800

    优化：视频文件预览支持FTP

--- jodconverter-web/src/main/java/cn/keking/service/impl/MediaFilePreviewImpl.java
+++ jodconverter-web/src/main/java/cn/keking/service/impl/MediaFilePreviewImpl.java
@@ -1,7 +1,11 @@
 package cn.keking.service.impl;
 
 import cn.keking.model.FileAttribute;
+import cn.keking.model.ReturnResponse;
 import cn.keking.service.FilePreview;
+import cn.keking.utils.DownloadUtils;
+import cn.keking.utils.FileUtils;
+import cn.keking.web.filter.BaseUrlFilter;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 /**
@@ -13,8 +17,31 @@ import org.springframework.ui.Model;
 @Service
 public class MediaFilePreviewImpl implements FilePreview {
 
+    private final DownloadUtils downloadUtils;
+
+    private final FileUtils fileUtils;
+
+    public MediaFilePreviewImpl(DownloadUtils downloadUtils,
+                                FileUtils fileUtils) {
+        this.downloadUtils = downloadUtils;
+        this.fileUtils = fileUtils;
+    }
+
     @Override
     public String filePreviewHandle(String url, Model model, FileAttribute fileAttribute) {
+        // 不是http开头，浏览器不能直接访问，需下载到本地
+        if (url != null && !url.toLowerCase().startsWith("http")) {
+            ReturnResponse<String> response = downloadUtils.downLoad(fileAttribute, fileAttribute.getName());
+            if (0 != response.getCode()) {
+                model.addAttribute("fileType", fileAttribute.getSuffix());
+                model.addAttribute("msg", response.getMsg());
+                return "fileNotSupported";
+            } else {
+                model.addAttribute("mediaUrl", BaseUrlFilter.getBaseUrl() + fileUtils.getRelativePath(response.getContent()));
+            }
+        } else {
+            model.addAttribute("mediaUrl", url);
+        }
         model.addAttribute("mediaUrl", url);
         String suffix=fileAttribute.getSuffix();
         if ("flv".equalsIgnoreCase(suffix)) {
