45eaf5bbaa9ec4dcbcc84fdc3dbae21a1d9e3cf6
chenkailing <632104866@qq.com>
2020-12-26 02:10:50 +0800

    设置pdfbox兼容低版本jdk

--- jodconverter-web/src/main/java/cn/keking/FilePreviewApplication.java
+++ jodconverter-web/src/main/java/cn/keking/FilePreviewApplication.java
@@ -9,7 +9,14 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @ComponentScan(value = "cn.keking.*")
 public class FilePreviewApplication {
+
 	public static void main(String[] args) {
+		FilePreviewApplication.staticInitSystemProperty();
         SpringApplication.run(FilePreviewApplication.class, args);
+	}
+
+	private static void staticInitSystemProperty(){
+		//pdfbox兼容低版本jdk
+		System.setProperty("sun.java2d.cmm", "sun.java2d.cmm.kcms.KcmsServiceProvider");
 	}
 }
