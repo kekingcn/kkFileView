d61e5236d0941c1841fac9a7d1f83989dca2e9d0
kl <chenkailing@xd.com>
2021-02-04 11:33:56 +0800

    新增启动完成，打印启动耗时、演示页访问地址

--- server/src/main/java/cn/keking/ServerMain.java
+++ server/src/main/java/cn/keking/ServerMain.java
@@ -1,22 +1,35 @@
 package cn.keking;
 
-import org.springframework.boot.SpringApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.util.StopWatch;
 
 @SpringBootApplication
 @EnableScheduling
 @ComponentScan(value = "cn.keking.*")
 public class ServerMain {
 
+	private static final Logger logger = LoggerFactory.getLogger(ServerMain.class);
+
 	public static void main(String[] args) {
+		StopWatch stopWatch = new StopWatch();
+		stopWatch.start();
 		ServerMain.staticInitSystemProperty();
-        SpringApplication.run(ServerMain.class, args);
+		new SpringApplicationBuilder(ServerMain.class)
+				.logStartupInfo(false)
+				.run(args);
+		stopWatch.stop();
+		logger.info("kkFileView 服务启动完成，耗时:{}s，演示页请访问: http://127.0.0.1:8012 ",stopWatch.getTotalTimeSeconds() );
+
 	}
 
 	private static void staticInitSystemProperty(){
 		//pdfbox兼容低版本jdk
 		System.setProperty("sun.java2d.cmm", "sun.java2d.cmm.kcms.KcmsServiceProvider");
 	}
+
 }
