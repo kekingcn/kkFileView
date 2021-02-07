679459f53b84e30eb067e087b68ae3f0d0f763e0
kl <chenkailing@xd.com>
2021-02-07 18:34:35 +0800

    更新 spring-boot 到最新的 v2.4.2 版本

--- server/src/test/java/cn/keking/ServerMainTests.java
+++ server/src/test/java/cn/keking/ServerMainTests.java
@@ -1,16 +1,13 @@
 package cn.keking;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
-public class ServerMainTests {
+ class ServerMainTests {
 
 	@Test
-	public void contextLoads() {
+	 void contextLoads() {
 	}
 
 }
