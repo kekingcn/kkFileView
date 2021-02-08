4f4e75859d3304f8e96dd9d9988b70557f46d6c0
kl <chenkailing@xd.com>
2021-02-08 15:16:24 +0800

    新增 kkFIleView 的 banner 信息

--- server/src/main/java/cn/keking/ServerMain.java
+++ server/src/main/java/cn/keking/ServerMain.java
@@ -1,8 +1,10 @@
 package cn.keking;
 
+import cn.keking.config.AppBanner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
@@ -21,9 +23,10 @@ public class ServerMain {
         stopWatch.start();
         ConfigurableApplicationContext context = new SpringApplicationBuilder(ServerMain.class)
                 .logStartupInfo(false)
+                .banner(new AppBanner())
                 .run(args);
         stopWatch.stop();
-        String port = context.getEnvironment().getProperty("server.port");
+        Integer port = context.getBean(ServerProperties.class).getPort();
         logger.info("kkFileView 服务启动完成，耗时:{}s，演示页请访问: http://127.0.0.1:{} ", stopWatch.getTotalTimeSeconds(), port);
     }
 
