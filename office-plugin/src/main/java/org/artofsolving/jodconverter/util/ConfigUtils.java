b9ea2132a7f5269c2c047ef1108073a8569e1ace
Yiding He <yiding.he@gmail.com>
2021-08-23 18:25:43 +0800

    修复 IntelliJ 运行单元测试失败的问题

--- office-plugin/src/main/java/org/artofsolving/jodconverter/util/ConfigUtils.java
+++ office-plugin/src/main/java/org/artofsolving/jodconverter/util/ConfigUtils.java
@@ -9,6 +9,7 @@ import java.io.File;
 public class ConfigUtils {
 
     private static final String MAIN_DIRECTORY_NAME = "server";
+
     private static final String OFFICE_PLUGIN_NAME = "office-plugin";
 
     public static String getHomePath() {
@@ -21,7 +22,7 @@ public class ConfigUtils {
         } else {
             String separator = File.separator;
             if (userDir.contains(MAIN_DIRECTORY_NAME)) {
-                userDir = userDir + separator + "src" + separator +  "main";
+                userDir = userDir + separator + "src" + separator + "main";
             } else {
                 userDir = userDir + separator + MAIN_DIRECTORY_NAME + separator + "src" + separator + "main";
             }
@@ -29,21 +30,43 @@ public class ConfigUtils {
         return userDir;
     }
 
+    // 获取环境变量，如果找不到则返回默认值
+    @SuppressWarnings("SameParameterValue")
+    private static String env(String key, String def) {
+        String value = System.getenv(key);
+        return value == null ? def : value;
+    }
 
-    public static String getOfficePluginPath() {
-        String userDir = System.getenv("KKFILEVIEW_BIN_FOLDER");
-        if (userDir == null) {
-            userDir = System.getProperty("user.dir");
-        }
-        if (userDir.endsWith("bin")) {
-            userDir = userDir.substring(0, userDir.length() - 4);
-        } else {
-            String separator = File.separator;
-            if (!userDir.contains(OFFICE_PLUGIN_NAME)) {
-                userDir = userDir + separator + OFFICE_PLUGIN_NAME;
+    // 返回参数列表中第一个真实存在的路径，或者 null
+    private static String firstExists(File... paths) {
+        for (File path : paths) {
+            if (path.exists()) {
+                return path.getAbsolutePath();
             }
         }
-        return userDir;
+        return null;
+    }
+
+    public static String getOfficePluginPath() {
+        String userDir = System.getProperty("user.dir");
+        String binFolder = env("KKFILEVIEW_BIN_FOLDER", userDir);
+
+        File pluginPath = new File(binFolder);
+
+        // 如果指定了 bin 或其父目录，则返回父目录
+        // 否则在当前目录和父目录中寻找 office-plugin
+        if (new File(pluginPath, "bin").exists()) {
+            return pluginPath.getAbsolutePath();
+
+        } else if (pluginPath.exists() && pluginPath.getName().equals("bin")) {
+            return pluginPath.getParentFile().getAbsolutePath();
+
+        } else {
+            return firstExists(
+                    new File(pluginPath, OFFICE_PLUGIN_NAME),
+                    new File(pluginPath.getParentFile(), OFFICE_PLUGIN_NAME)
+            );
+        }
     }
 
     public static String getCustomizedConfigPath() {
