976e54407ef191ec2537bfae4e973582e830a8ff
kl <632104866@QQ.com>
2023-03-03 18:42:44 +0800

    Update documentation (#437)

--- server/src/main/bin/startup.sh
+++ server/src/main/bin/startup.sh
@@ -53,7 +53,7 @@ else
   echo "Starting kkFileView..."
   nohup java -Dfile.encoding=UTF-8 -Dspring.config.location=../config/application.properties -jar kkFileView-4.2.0-SNAPSHOT.jar > ../log/kkFileView.log 2>&1 &
   echo "Please execute ./showlog.sh to check log for more information"
-  echo "You can get help in our official home site: https://kkFileView.keking.cn"
+  echo "You can get help in our official home site: https://kkview.cn"
   echo "If you need further help, please join our kk opensource community: https://t.zsxq.com/09ZHSXbsQ"
   echo "If this project is helpful to you, please star it on https://gitee.com/kekingcn/file-online-preview/stargazers"
   PROCESS=$(ps -ef | grep kkFileView | awk 'NR==1{print $2}')
--- server/src/main/resources/web/comment.ftl
+++ server/src/main/resources/web/comment.ftl
@@ -30,7 +30,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="https://kkfileview.keking.cn" target='_blank'>kkFileView</a>
+          <a class="navbar-brand" href="https://kkview.cn" target='_blank'>kkFileView</a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
           <ul class="nav navbar-nav">
