724a0124b52ee0a1a1d594d326607043669df01f
kl <632104866@QQ.com>
2023-03-03 18:58:32 +0800

    Update the installation resource address (#438)
    * Update documentation
    
    * Update the installation resource address

--- server/src/main/bin/install.sh
+++ server/src/main/bin/install.sh
@@ -2,7 +2,7 @@
 cd /tmp
 
 install_redhat() {
-   wget https://kkfileview.keking.cn/LibreOffice_7.3.7_Linux_x86-64_rpm.tar.gz -cO LibreOffice_7_rpm.tar.gz && tar -zxf /tmp/LibreOffice_7_rpm.tar.gz && cd /tmp/LibreOffice_7.3.7.2_Linux_x86-64_rpm/RPMS
+   wget https://kkview.cn/resource/LibreOffice_7.3.7_Linux_x86-64_rpm.tar.gz -cO LibreOffice_7_rpm.tar.gz && tar -zxf /tmp/LibreOffice_7_rpm.tar.gz && cd /tmp/LibreOffice_7.3.7.2_Linux_x86-64_rpm/RPMS
    echo $?
    if [ $? -eq 0 ];then
      yum install -y libSM.x86_64 libXrender.x86_64  libXext.x86_64
@@ -15,7 +15,7 @@ install_redhat() {
 }
 
 install_ubuntu() {
-   wget  https://kkfileview.keking.cn/LibreOffice_7.3.7_Linux_x86-64_deb.tar.gz  -cO LibreOffice_7_deb.tar.gz && tar -zxf /tmp/LibreOffice_7_deb.tar.gz && cd /tmp/LibreOffice_7.3.7.2_Linux_x86-64_deb/DEBS
+   wget  https://kkview.cn/resource/LibreOffice_7.3.7_Linux_x86-64_deb.tar.gz  -cO LibreOffice_7_deb.tar.gz && tar -zxf /tmp/LibreOffice_7_deb.tar.gz && cd /tmp/LibreOffice_7.3.7.2_Linux_x86-64_deb/DEBS
    echo $?
  if [ $? -eq 0 ];then
      apt-get install -y libxinerama1 libcairo2 libcups2 libx11-xcb1
