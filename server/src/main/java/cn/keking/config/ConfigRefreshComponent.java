44992687f81e590252ddee29a58b59c1e77690eb
高雄 <admin@cxcp.com>
2023-04-27 09:34:46 +0000

    规范命名规则 规范命名规则
    Signed-off-by: 高雄 <admin@cxcp.com>

--- server/src/main/java/cn/keking/config/ConfigRefreshComponent.java
+++ server/src/main/java/cn/keking/config/ConfigRefreshComponent.java
@@ -88,7 +88,7 @@ public class ConfigRefreshComponent {
                     BeiAn = properties.getProperty("BeiAn", ConfigConstants.DEFAULT_BeiAn_DISABLE);
                     prohibit = properties.getProperty("prohibit", ConfigConstants.DEFAULT_prohibit_DISABLE);
                     password = properties.getProperty("sc.password", ConfigConstants.DEFAULT_password_DISABLE);
-                    pdf2JpgDpi = Integer.parseInt(properties.getProperty("pdf2jpg.dpi", ConfigConstants.DEFAULT_PDF2_JPG_DPI_SIZE_DISABLE));
+                    pdf2JpgDpi = Integer.parseInt(properties.getProperty("pdf2jpg.dpi", ConfigConstants.DEFAULT_PDF2_JPG_DPI_DISABLE));
                     deletesourcefile =  Boolean.parseBoolean(properties.getProperty("delete.source.file", ConfigConstants.DEFAULT_Delete_Source_File_PREVIEW_TYPE));
                     prohibitArray = prohibit.split(",");
 
