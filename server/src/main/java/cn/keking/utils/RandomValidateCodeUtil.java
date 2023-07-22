da5dac73ab6c89778efb2732d7c6e2afb4c72b34
gaoxiongzaq <admin@cxcp.com>
2023-07-22 08:53:50 +0800

    验证码方法 代码优化

--- server/src/main/java/cn/keking/utils/RandomValidateCodeUtil.java
+++ server/src/main/java/cn/keking/utils/RandomValidateCodeUtil.java
@@ -63,13 +63,13 @@ public class RandomValidateCodeUtil {
             blue = random.nextInt(255);
             // 用随机产生的颜色将验证码绘制到图像中。
             gd.setColor(new Color(red, green, blue));
-            gd.drawString(sessionCode, xx, codeY);
+            gd.drawString(sessionCode, 18, codeY);
             randomCode.append(sessionCode);
         }else {
             // 随机产生codeCount数字的验证码。
             for (int i = 0; i < codeCount; i++) {
                 // 得到随机产生的验证码数字。
-                String code = String.valueOf(codeSequence[random.nextInt(30)]);
+                String code = String.valueOf(codeSequence[random.nextInt(52)]);
                 // 产生随机的颜色分量来构造颜色值，这样输出的每位数字的颜色值都将不同。
                 red = random.nextInt(255);
                 green = random.nextInt(255);
