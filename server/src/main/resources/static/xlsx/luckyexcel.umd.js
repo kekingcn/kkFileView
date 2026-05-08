6d037f421c63900d2562d70e0cf9151c67edeb71
何武德 <hewude@ruijie.com.cn>
2026-05-08 16:35:11 +0800

    fix(xlsx): simplify matchType mapping logic in LuckySheet class

--- server/src/main/resources/static/xlsx/luckyexcel.umd.js
+++ server/src/main/resources/static/xlsx/luckyexcel.umd.js
@@ -4434,7 +4434,7 @@ var LuckySheet = /** @class */function (_super) {
       var _hintShow = _hint ? true : false;
       var matchType = constant_1.COMMON_TYPE2.includes(_type) ? "common" : _type;
       var _type2Map = constant_1.DATA_VERIFICATION_TYPE2_MAP[matchType];
-      _type2 = operator ? ((_type2Map && _type2Map[operator] != null) ? _type2Map[operator] : "bw") : "bw";
+      _type2 = operator ? (_type2Map ? _type2Map[operator] : "bw") : "bw";
       // mobile phone number processing
       if (_type === "text_content" && ((_value1 === null || _value1 === void 0 ? void 0 : _value1.includes("LEN")) || (_value1 === null || _value1 === void 0 ? void 0 : _value1.includes("len"))) && (_value1 === null || _value1 === void 0 ? void 0 : _value1.includes("=11"))) {
         _type = "validity";
